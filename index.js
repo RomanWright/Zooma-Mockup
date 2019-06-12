bfb1eea94482a9ed70c21fce8e001fd6cc91094b
Roman Wright <rwright@berkeley.edu>
2019-06-12 14:43:57 -0700

    serves image

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(__dirname + '/resources'));
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join('index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(8080);
